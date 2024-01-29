866a180a83fbc6143a421cdf80e159101716e61e
kdjun97 <kdjun97@gmail.com>
2024-01-29 19:49:57 +0900

    init project

---
+++ PrographyAssignment/PrographyAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrographyAssignment
+//
+//  Created by 김동준 on 1/29/24
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrographyAssignment/PrographyAssignment/PrographyAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrographyAssignmentApp.swift
+//  PrographyAssignment
+//
+//  Created by 김동준 on 1/29/24
+//
+
+import SwiftUI
+
+@main
+struct PrographyAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
